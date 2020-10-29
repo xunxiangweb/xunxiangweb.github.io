631e0726e53dd9007de205999080f0097e826f8f
Oliver Chen <56765896+YushunChen@users.noreply.github.com>
2020-10-28 20:10:23 -0500

    General update
    updates to lianxisheng page mainly

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(event){
     // array with texts to type in typewriter
     var dataText = [ "帮助留学生了解未来的无数种可能", "连接起校园和职场", "让经验和知识不断传递"];
-    
+
     // type one text in the typwriter
     // keeps calling itself until the text is finished
     function typeWriter(text, i, fnCallback) {
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded',function(event){
       if (i < (text.length)) {
         // add next character to h1
        document.querySelector("#typing-text").innerHTML = text.substring(0, i+1) +'<span id="typing-span" aria-hidden="true"></span>';
-  
+
         // wait for a while and call this function again for next character
         setTimeout(function() {
           typeWriter(text, i + 1, fnCallback)
@@ -40,3 +40,24 @@ document.addEventListener('DOMContentLoaded',function(event){
     // start the text animation
     StartTextAnimation(0);
   });
+
+  var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
